98f4621992b3fb4e3966df24bb15460b589a104c
ferdisaltek <ferdisaltek@gmail.com>
2024-05-23 23:26:23 +0300

    variable

---
+++ valuedecleration.go
@@ -0,0 +1,31 @@
+/*package main
+
+import (
+	"fmt"
+)
+
+var name string
+var age int
+
+func main() {
+	name = "john"
+	age = 19
+	fmt.Println(name, age)
+}
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a, b = 6, "Hello"
+	c, d := 7, "World!"
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+}
